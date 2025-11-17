131f699105c571a4387605ce5bda6cd1a897c2c1
shixu <sgyt123123@gmail.com>
2025-11-18 07:12:10 +0800

    chore: 移除项目文件与代码库，清理遗留代码与资源

--- setupTests.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom/vitest'
-
-class MockIntersectionObserver {
-  observe() {}
-  disconnect() {}
-  unobserve() {}
-}
-// @ts-expect-error test environment polyfill
-global.IntersectionObserver = MockIntersectionObserver
